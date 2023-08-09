bf78178c8aaddf05ad6b11f2cbc34617a935ae0c
Emmanuel Xs <emmanuelnwaohiri1@gmail.com>
2023-08-09 17:28:54 +0100

    added tests for the package

---
+++ jest.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  testEnvironment: "node", // Specify the test environment (node or jsdom)
+  testRegex: "tests/.*\\.test\\.js$", // Specify the regex pattern for test files
+};
---
+++ tests/geez.js
@@ -0,0 +1,6 @@
+const getServiceProvider = require("../package/index");
+// const removeCountryCode = require("../package/index");
+
+const result = getServiceProvider("+2349118336065");
+
+console.log(result);
